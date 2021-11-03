5a08a0fec350dfa788f4bf562b356c75eac53647
unknown <nsyawali12@gmail.com>
2021-11-03 18:08:58 +0700

    merge file

---
+++ searchable_v1.py
@@ -0,0 +1,57 @@
+import numpy
+import easyocr
+import pdf2image
+import pytesseract
+import cv2
+import PIL
+
+def image_conversion(inpath, image_path):
+    print("Converting to image")
+    OUTPUT_FOLDER = None
+    FIRST_PAGE = None
+    LAST_PAGE = None
+    FORMAT = 'jpg'
+    USERPWD = None
+    USE_CROPBOX = False
+    STRICT = False
+
+    pil_images = pdf2image.convert_from_path(inpath,
+                                             output_folder = OUTPUT_FOLDER,
+                                             first_page=FIRST_PAGE,
+                                             last_page=LAST_PAGE,
+                                             userpw=USERPWD,
+                                             use_cropbox=USE_CROPBOX,
+                                             strict=STRICT)
+    
+
+    for image in pil_images:
+        image.save(image_path+'img_homeland_converted.jpg')
+        
+# inpat sample pdf
+# inpath = "C:/Users/Wallsk/Documents/working-stuff/Projects/searchable_PDF/inpath/sample.pdf"
+
+# inpath coba data tanah
+
+inpath = "C:/Users/Wallsk/Documents/working-stuff/Projects/searchable_PDF/inpath/data1.PDF"
+image_path = "C:/Users/Wallsk/Documents/working-stuff/Projects/searchable_PDF/image_path/"
+image_conversion(inpath, image_path)
+
+""" Convert to PDF """
+
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C://Program Files//Tesseract-OCR//tesseract.exe'
+TESSDATA_PREFIX = 'C://Program Files//Tesseract-OCR'
+tessdata_dir_config = '--tessdata-dir "C://Program Files//Tesseract-OCR//tessdata"'
+
+input_dir = "C:/Users/Wallsk/Documents/working-stuff/Projects/searchable_PDF/image_path/img_homeland_converted.jpg"
+
+img = cv2.imread(input_dir, 1)
+
+result = pytesseract.image_to_pdf_or_hocr(img, lang="eng",
+                                            config=tessdata_dir_config)
+
+f = open("C:/Users/Wallsk/Documents/working-stuff/Projects/searchable_PDF/output_path/homeland_searchablePDF.pdf", "w+b")
+f.write(bytearray(result))
+f.close()
